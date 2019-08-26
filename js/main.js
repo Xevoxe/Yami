711a553c310befda012a94d3d421d981c54c12f4
John Doe <xevoxe@gmail.com>
2019-08-26 12:12:41 -0500

    Sripped Reload Stuff for mobile Bug

--- js/main.js
+++ js/main.js
@@ -1,22 +1,7 @@
 
 //
 document.addEventListener("DOMContentLoaded", function(){
-    // Handler when the DOM is fully loaded
-document.read
-let resizeId;
 
-window.onresize= ()=> {
-    clearTimeout(resizeId);
-    resizeId= setTimeout(resizeReload,250);
-};
-
-window.onbeforeunload = ()=>{
-    // window.scrollTo(0,0);
-}
-
-const resizeReload = ()=> {
-    location.reload(true);
-}
 const stickyElement = (pos,element) =>{
     let stickyPos = document.getElementById(element).offsetTop - document.documentElement.clientHeight * pos;
     let elmt = document.getElementById(element);
